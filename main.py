b3b80edc4e00e7f588c8dbd5a0692b421119140d
AndreasNel <nel.andreas1@gmail.com>
2018-09-24 17:19:23 +0200

    modified to use active learning by removing correctly classified patterns

--- main.py
+++ main.py
@@ -91,23 +91,31 @@ def train_neural_network(x):
                 "train_accuracy": 0,
                 "epoch_loss": [],
             }
-            
+
+            correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+            train_features = data["train_features"][:]
+            train_labels = data["train_labels"][:]
             for epoch in range(hm_epochs):
                 epoch_loss = 0
                 i = 0
-                while i < len(data["train_features"]):
+                while i < len(train_features):
                     start = i
                     end = i + batch_size
-                    batch_x = np.array(data["train_features"][start:end])
-                    batch_y = np.array(data["train_labels"][start:end])
+                    batch_x = np.array(train_features[start:end])
+                    batch_y = np.array(train_labels[start:end])
                     i += batch_size
                     # c is the cost
                     _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
                     epoch_loss += c
                 run_results["epoch_loss"].append(epoch_loss)
+                print("Using {} instances...".format(len(train_features)))
                 print("Epoch {} completed out of {}, loss: {}".format(epoch, hm_epochs, epoch_loss))
+                new_train_features = np.array(train_features)
+                new_train_labels = np.array(train_labels)
+                success = correct.eval(feed_dict={x: new_train_features, y: new_train_labels})
+                train_features = np.asarray(new_train_features[~success])
+                train_labels = np.asarray(new_train_labels[~success])
 
-            correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
             # Check the test data against the trained network
             accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
             run_results["test_accuracy"] = accuracy.eval({x: data["test_features"], y: data["test_labels"]})
